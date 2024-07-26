f005812d409216e9b08ed8a0291d16b39bdbdbce
Devin Jaggernauth <neexis@gmail.com>
2024-07-26 16:11:58 -0400

    implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase}
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -19,4 +21,26 @@ class User < ApplicationRecord
 
   end
 
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+    remember_digest
+  end
+
+  def authenticated? (remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  def session_token
+    remember_digest || remember
+  end
 end
